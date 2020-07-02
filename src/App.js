74e4a14d5dfed426093ef09d409d3625a46ccafa
DianaYJC <51901766+DianaYJC@users.noreply.github.com>
2020-07-02 00:11:54 -0700

    creating sider data

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import HeaderNav from './pages/HeaderNav';
 import ContentContainer from './pages/ContentContainer';
 import { Layout, Menu, Button, Tag, Tabs } from 'antd';
 import 'antd/dist/antd.css';
+import CollapsePanel from 'antd/lib/collapse/CollapsePanel';
 
 const { Sider, Content } = Layout;
-const { TabPane } = Tabs;
+const { TabPane } = Tabs
+
+const initialPanes = [
+  { title: '首页', content: '', key: '首页', closable: false },
+  { title: '总经办', content: '', key: '总经办' },
+  { title: '企划部 3', content: 'Content of Tab 3', key: '3' },
+];
 
 export default class App extends Component {
   state = {
@@ -21,12 +28,64 @@ export default class App extends Component {
       { range: '2020年第二季度（456月）' },
       { range: '2020年第二季度（456月）' },
     ],
-    tabs:[
-      {title:'首页', content:'', key:'首页', closable: false},
-      {title:'总经办', content:'', key:'总经办'},
+    activeKey: initialPanes[0].key, //默认focus的Tabpane[]
+    panes: initialPanes,
+    departments:[
+      {title:'人力行政部',key:'人力行政部', 
+      members:[
+        {name:'Diana',key:'Diana'},
+        {name:'Jack',key:'Jack'},
+        {name:'King',key:'King'},
+        {name:'Ida',key:'Ida'},
+        {name:'Summer',key:'Summer'},
+
+      ]},
+      {title:'企业部',key:'企业部'}, 
+      {title:'品部',key:'品部'},
+      {title:'财务部',key:'财务部'},
+      {title:'市场部',key:'市场部'},
     ]
   
   }
+
+  // Tab 
+  onChange = (activeKey) => {    //onChange Tab focus change 
+    this.setState({
+      activeKey
+    })
+  };
+
+  add = () => { };
+  onEdit = (targetKey, action) => {
+    console.log(targetKey, action)
+    this[action](targetKey);
+  };
+  remove = targetKey => {
+    console.log(targetKey)
+    const { panes, activeKey } = this.state;
+    let newActiveKey = activeKey;
+    let lastIndex;
+    panes.forEach((pane, i) => {
+      if (pane.key === targetKey) {
+        lastIndex = i - 1;
+      }
+    });
+    const newPanes = panes.filter(pane => pane.key !== targetKey);
+    if (newPanes.length && newActiveKey === targetKey) {
+      if (lastIndex >= 0) {
+        newActiveKey = newPanes[lastIndex].key;
+      } else {
+        newActiveKey = newPanes[0].key;
+      }
+    }
+    this.setState({
+      panes: newPanes,
+      activeKey: newActiveKey,
+    });
+  };
+
+
+
 
   toggle = () => {
     this.setState({
@@ -37,12 +96,12 @@ export default class App extends Component {
 
   render() {
 
-    const { collapsed, tabs } = this.state;
+    const { collapsed, tabs, panes, activeKey, departments } = this.state;
     return (
       <Layout>
         <div></div>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <SiderNav />
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed} >
+          <SiderNav departments={departments} />
 
         </Sider>
         <Layout className="site-layout">
@@ -50,17 +109,13 @@ export default class App extends Component {
           <HeaderNav collapsed={collapsed} toggle={this.toggle} />
 
 
-          <Tabs defaultActiveKey="首页">
-            <TabPane tab="首页" key="1" closable=' false' />
-            <TabPane tab="总经办" key="2" closable={true} />
-            {tabs.map(pane => (
-          <TabPane tab={pane.title} key={pane.key} closable={pane.closable}>
-            {pane.content}
-          </TabPane>
-        ))}
+          <Tabs  size='small' type="editable-card" hideAdd onChange={this.onChange}  onEdit={this.onEdit} activeKey={activeKey} tabBarGutter={0}>
+            {panes.map(pane => (
+              <TabPane tab={pane.title} key={pane.key} closable={pane.closable} style={{width:'12rem'}} >
+                {pane.content}
+              </TabPane>
+            ))}
           </Tabs>
-
-
 
 
           <ContentContainer expandAllRow={this.state.expandAllRow} tableData={this.state.tableData} />
